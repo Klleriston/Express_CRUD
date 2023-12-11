ded5249ce2570ee74c135883a0558500dc767701
Klleriston A <klleandrade@outlook.com>
2023-12-11 17:50:38 -0300

    melhorando o crud

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 const express = require('express');
 const app = express();
-app.use(express.json())
+app.use(express.json());
+const users = [];
 
 app.get('/usuarios', (req, res) => {
     console.log('api get usuario')
-
-    res.status(200).send('todos os usuarios encontrados !')
+    res.status(200).send(users)
 })
 app.get('/usuarios/:id', (req, res) => {
     console.log('api get id usuario')
-    res.status(200).send(`o usuario foi econtrado ! ${req.params.id}`)
+    const id = users.find(x => x.id == req.params.id)
+    res.status(200).send(id)
 })
 app.post('/usuarios/cadastrar', (req, res) => {
+    users.push(req.body)
     console.log('api post')
-    res.status(200).send(req.body)
+    res.status(200).send('usuario cadastrado')
 })
 app.put('/usuarios/editar/:id', (req, res) => {
-    res.status(200).send(req.params.id)
+    const id = users.find(x => x.id == req.params.id)
+    const usuarioID = users.indexOf(id)
+    users[usuarioID] = req.body
+    res.status(200).send('aluno atualizado!')
     console.log('api put usuario')
 
 })
 app.delete('/usuarios/delete/:id', (req, res) => {
+    const id = users.find(x => x.id == req.params.id)
+    const usuarioID = users.indexOf(id)
+    users.slice(usuarioID, 1)
     res.status(200).send('Usuario deletado !')
     console.log('api delete usuario')
 
